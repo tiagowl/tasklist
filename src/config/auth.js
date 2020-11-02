3d84465f2b668633b54d38f1cfe1b7e67b360ded
tiago <winkellandi123@gmail.com>
2020-11-02 13:08:41 -0300

    first commit

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'adbf5a778175ee757c34d0eba4e932bc',
+  expiresIn: '7d',
+};
---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+// configurações do banco de dados
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: '12qwaszx#$',
+  database: 'tasklist',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
