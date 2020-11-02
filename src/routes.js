183bfb21585d11bdae7272ad74b734d5142e9ba6
tiago <winkellandi123@gmail.com>
2020-11-02 17:22:23 -0300

    cadastrar, editar e excluir tasks

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import TaskController from './app/controllers/TaskController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
@@ -12,5 +13,9 @@ routes.use(authMiddleware);
 
 // todas as rotas abaixo do middleware precisam estar autenticados
 routes.put('/users', UserController.update);
+routes.post('/tasks', TaskController.store);
+routes.get('/tasks', TaskController.index);
+routes.put('/tasks/:task_id', TaskController.update);
+routes.delete('/tasks/:task_id', TaskController.delete);
 
 export default routes;
